2f199b1664ce360599ad7b35efc7e44162968b83
Gabriel Gonçalves Corvelo Bastos <57567691+bastosgabriel312@users.noreply.github.com>
2021-01-12 18:47:48 -0300

    12_01_2021
    Adicionado o GUI utilizando a biblioteca PySimpleGUI e concluída a lógica em pandas para tratamento do Dataset de IPANEMA

--- app.py
+++ app.py
@@ -1,33 +1,121 @@
-from arquivo_sms import *
+import PySimpleGUI as sg
+from pandas_back import *
+
+
 
 data = d_mais_um().strftime("%d.%m")
+
+#Frases SMS 
 frase_sp1 = 'SEU COMPROMISSO COM SEM PARAR VENCE HOJE! Devido atual cenario de preferencia aos canais digitais, APP e Internet Banking. Tel 40202350 Whats 11 969180729.'
 frase_sp2 = "SEU COMPROMISSO COM O SEM PARAR VENCE DIA "+data+"!Devido atual cenario de preferencia aos canais digitais, APP e Internet Banking.Tel.40202350 Whats 11969180729."
+frase_ipanema = "SEU COMPROMISSO COM A IPANEMA VENCE HOJE! Devido atual cenario de preferencia aos canais digitais, APP e Internet Banking. Tel 08000258961 Whats 11 961731064."
+frase_ipanemad1 = "SEU COMPROMISSO COM A IPANEMA VENCE DIA "+data+"!Devido atual cenario de preferencia aos canais digitais, APP e Internet Banking.Tel.40202350 Whats 11969180729."
 
-#sms_semparar('sms',frase_sp1)
-print("ENVIO DE BOLETO VIA SMS \n")
-print("ESCOLHA UMA OPÇÃO: \n 1. Sem Parar \n 2. Sem Parar D+1 \n 3. Ipanema \n 0. Outra Frase")
+#formatação de data atual concatenada (DiaMesAno)
 hoje = date.today()
 hoje = hoje.strftime("%d%m%Y")
 
+#Define Tema
+sg.theme('DarkGrey4') 
+
+# Define os conteúdos no layout 
+layout = [[sg.Text(('CONVERSOR SMS'), size=(20, 1), justification='center', font=('Courier 12', 25))],
+            #FRAME 1 - FRASES
+            [sg.Frame(layout=[[sg.Radio('   Sem parar', "RADIO1",key='semparar', default=True, size=(15,1))],
+            [sg.Radio('Sem parar D+1', "RADIO1",key='semparard1')],
+            [sg.Radio('Ipanema', "RADIO1",key='ipanema')],
+            [sg.Radio('Ipanema D+1', "RADIO1",key='ipanemad1')],
+            [sg.Radio("Outro: ","RADIO1", key='rdoutro'), sg.Input(key='outro', size=(41,1))]],
+                title='Frase',title_color='red', size=(41,1))],
+        
+            #FRAME 2 - ARQUIVO
+            [sg.Frame(layout=[
+                [sg.Input(size=(31,1),key='carquivo'), sg.FileBrowse(key='arquivo')],
+                [sg.Text('Exportar:'),
+                    sg.Radio('.txt',"RADIO3", key='txt', size=(3,5)),
+                    sg.Radio('.csv',"RADIO3",  key='csv', default=True, size=(27,5)) ],
+            [sg.FolderBrowse(initial_folder ='/' , button_text = "Selecionar Pasta", key='export'),sg.Text(size=(30,1), key='-OUTPUT-')]],
+            title='Arquivo', title_color = 'red')],
+            [sg.Button('Ok'), sg.Button('Sair')]]
+
+          
+#Criar layout da tela
+window = sg.Window('Arquivo SMS', layout)
+
+# Looping para interações na tela do app
 while True:
-    try:
-        a = int(input("Digite um número: "))
-        if a == 1:
-            nome_arquivo = ""+hoje+"_PREVENTIVA_SEMPARAR_SHORT_1"
-            dataset = sms_semparar('sms',frase_sp1)
-            exportar_dados(dataset, nome_arquivo)
-        elif a == 2:
-            dataset = sms_semparar('sms',frase_sp2)
-            nome_arquivo = ""+hoje+"_PREVENTIVA_SEMPARAR_SHORT_2"
-            sms_semparar('sms',frase_sp1)
-            exportar_dados(dataset, nome_arquivo)
-        elif a == 3:
-            print("Ainda Não disponivel")
-        elif a == 0:
-            print("Digite a Frase:")
+    event, values = window.read()
+    # Ver quando a janela é fechada ou o evento é sair
+    if event == sg.WINDOW_CLOSED or event == 'Sair':
         break
-    except ValueError:
-        print('SOMENTE NUMEROS')
-    except PermissionError:
-        print("A planilha Temp deve permanecer fechada durante a execução")
+    # Output a message to the window
+    if event == 'Ok':
+        #Visualização de eventos utilizados
+        '''
+        print('Semparar: ',values['semparar'])
+        print('Semparar D+1: ',values['semparard1'])
+        print('ipanema: ',values['ipanema'])
+        print('ipanema D+1: ',values['ipanemad1'])
+        print('Outro: ',values['rdoutro'])
+        print('Input outro: ',values['outro'])  
+        print('Arquivo browse: ',values['arquivo'])
+        print('Arquivo input: ',values['carquivo'])
+        print('Txt: ',values['txt'])
+        print('csv: ',values['csv'])
+        print('arquivo export', values['export'])
+        '''
+
+        #Verifica se o campo de arquivo é diferente de vazio        
+        if values['arquivo']!='':
+            #Verifica se o valor selecionado txt e atribui a variavel tipo
+            if values['txt']:
+                tipo = 'txt'
+            #Verifica se o valor selecionado csv e atribui a variavel tipo caso não seja 'txt'
+            elif values['csv']:
+                tipo = 'csv'
+            #Veriica qual o layout/frase escolhido
+            if values['semparar']:
+                nome_arquivo = ""+hoje+"_PREVENTIVA_SEMPARAR_SHORT_1"
+                #Função de tratamento de dataset Sms Sem Parar proveniente de pandas_back
+                dataset = sms_semparar(values['arquivo'],frase_sp1)
+                #Função exportar dados proveniente de pandas_back
+                exportar_dados(dataset, nome_arquivo, tipo,values['export'])
+                sg.popup("Arquivo Exportado!!")
+            elif values['semparard1']:
+                nome_arquivo = ""+hoje+"_PREVENTIVA_SEMPARAR_SHORT_2"
+                #Função de tratamento de dataset Sms Sem Parar proveniente de pandas_back
+                dataset = sms_semparar(values['arquivo'],frase_sp2)
+                #Função exportar dados proveniente de pandas_back
+                exportar_dados(dataset, nome_arquivo,tipo,values['export'])
+                sg.popup("Arquivo Exportado!!")
+            elif values['ipanema']:
+                nome_arquivo = ""+hoje+"_PREVENTIVA_IPANEMA_SHORT"
+                #Função de tratamento de dataset Sms Ipanema proveniente de pandas_back
+                dataset = sms_ipanema(values['arquivo'],frase_ipanema)
+                #Função exportar dados proveniente de pandas_back
+                exportar_dados(dataset, nome_arquivo,tipo,values['export'])
+                sg.popup("Arquivo Exportado!!")
+            elif values['ipanemad1']:
+                nome_arquivo = ""+hoje+"_PREVENTIVA_IPANEMA_SHORT_2"
+                #Função de tratamento de dataset Sms Ipanema proveniente de pandas_back
+                dataset = sms_ipanema(values['arquivo'],frase_ipanemad1)
+                #Função exportar dados proveniente de pandas_back
+                exportar_dados(dataset, nome_arquivo,tipo,values['export'])
+                sg.popup("Arquivo Exportado!!")
+            elif values['rdoutro']:
+                nome_arquivo = ""+hoje+"_SMS"
+                #Função de tratamento de dataset Sms Sem Parar proveniente de pandas_back
+                dataset = sms_semparar(values['arquivo'],values['outro'])
+                #Função exportar dados proveniente de pandas_back
+                exportar_dados(dataset, nome_arquivo,tipo,values['export'])
+                sg.popup("Arquivo Exportado!!")
+            #Output que aparece assim que a pasta destino do arquivo exportado é escolhida
+            values['-OUTPUT-'] = values['export']
+
+                    
+            
+                
+                
+                
+#Fechar janela (caso saia do looping)       
+window.close()
--- pandas_back.py
+++ pandas_back.py
@@ -22,9 +22,12 @@ def contar_valores(dataset, coluna):
     data = pd.value_counts(dataset[coluna], dropna = False)
     return data
 
-def exportar_dados(nome,arquivo):
-    nome.to_csv(""+arquivo+".csv", index = False, header = False, sep = ';', encoding = 'utf-8')
-    
+def exportar_dados(nome,arquivo,tipo,pasta):
+    if tipo == 'csv':
+        nome.to_csv(""+pasta+"/"+arquivo+".csv", index = False, header = False, sep = ';', encoding = 'utf-8')
+    elif tipo == 'txt':
+        nome.to_csv(""+pasta+"/"+arquivo+".txt", index = False, header = False, sep = ';', encoding = 'utf-8')
+
 def contar_nulos_coluna(dataset, coluna):
     dados = pd.value_counts(dataset[coluna], dropna = False)
     return dados
@@ -46,13 +49,68 @@ def inserir_linha(idx, df, df_inserir):
 
 
 
+def tratamento_ipanema(data):
+
+    #Separando as colunas FONE em Dataframes diferentes
+    d1 = data['FONE1']
+    d2 = data['FONE2']
+    d3 = data['FONE3']
+    d4 = data['FONE4']
+    d5 = data['FONE5']
+    d6 = data['FONE6']
+    d7 = data['FONE7']
+
+    #Dataframe 1 recebe ao final do arquivo os outros dataframes
+    d1 = d1.append(d2, ignore_index=True)
+    d1 = d1.append(d3, ignore_index=True)
+    d1 = d1.append(d4, ignore_index=True)
+    d1 = d1.append(d5, ignore_index=True)
+    d1 = d1.append(d6, ignore_index=True)
+    d1 = d1.append(d7, ignore_index=True)
+
+    # Exclui as linhas nulas
+    d1 = d1.dropna()
+
+    # Transforma em string para retirar o .0 no final dos números
+    d1 = d1.astype(str)
+    df = d1.str.split('.')
+    df = df.str.get(0)
+    
+    return df
+
+def sms_ipanema(CSV,frase):
+    #Lê o CSV
+    data = pd.read_csv(CSV, sep=';')
+    #utiliza a função para tratar os dados no arquivo
+    data = tratamento_ipanema(data)
+    #Transforma de Pandas.Series em Pandas.DataFrame
+    data = data.to_frame()
+    #recebe arquivo de números teste para inclusão nas primeiras linhas da tabela
+    dic = dividir_coluna(num_teste('numteste'), ';',0)
+    #Transforma de Pandas.Series em Pandas.DataFrame
+    dic = dic.to_frame()
+    #Header da coluna recebe o nome '0'
+    data.columns = [0]
+    #função inserir linha para incluir os números no inicio da tabela
+    data = inserir_linha(0,data,dic)
+    #define duas colunas no dataframe: |1|frase|
+    data = pd.DataFrame(data, columns = [0,'frase'])
+    #todas as linhas da coluna frase recebem o parametro frase
+    data['frase'] = frase
+
+    return data
+    
+    
+
+
+
 def num_teste(arquivo):
     dataset = pd.read_fwf(""+arquivo+".txt", sep=';',dtype='unicode', header = None)
     return dataset
 
 def sms_semparar(arquivo,frase):
     #pandas lê o arquivo (.txt) e coloca dentro da variavel
-    dataset = pd.read_fwf(""+arquivo+".txt", sep=';',dtype='unicode', header = None)
+    dataset = pd.read_fwf(arquivo, sep=';',dtype='unicode', header = None)
     #retorno da função num_teste dentro de uma variavel lendo o arquivo 'numteste.txt'
     dic = num_teste('numteste')
     #Uso da função inserir_linha para mescar dois dataframes
@@ -79,4 +137,5 @@ def d_mais_um():
         
 
     return d_1
+
 
